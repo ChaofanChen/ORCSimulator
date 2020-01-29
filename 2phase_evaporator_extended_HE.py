9206d08c5c1a8d462a8d8e1ac5be18f811616b17
ChaofanChen <chaofan.chen@ufz.de>
2020-01-29 10:55:53 +0100

    adjust boundary conditions

--- 2phase_evaporator_extended_HE.py
+++ 2phase_evaporator_extended_HE.py
@@ -26,25 +26,28 @@ sink_s = sink('steam sink')
 sink_b = sink('brine sink')
 # connections
 # main cycle
-evaporator_wf_in = connection(source_wf, 'out1', evaporator, 'in3', m=243.72)
+evaporator_wf_in = connection(source_wf, 'out1', evaporator, 'in3')
 evaporator_wf_out = connection(evaporator, 'out3', sink_wf, 'in1')
 
 evaporator_steam_in = connection(source_s, 'out1', evaporator, 'in1')
 evaporator_sink_s = connection(evaporator, 'out1', sink_s, 'in1')
+
 evaporator_brine_in = connection(source_b, 'out1', evaporator, 'in2')
 evaporator_sink_b = connection(evaporator, 'out2', sink_b, 'in1')
-nw.add_conns(evaporator_wf_in, evaporator_wf_out, evaporator_steam_in, evaporator_sink_s, evaporator_brine_in, evaporator_sink_b)
+nw.add_conns(evaporator_wf_in, evaporator_wf_out)
+nw.add_conns(evaporator_steam_in, evaporator_sink_s)
+nw.add_conns(evaporator_brine_in, evaporator_sink_b)
 # parametrization of components
 evaporator.set_attr(pr1=0.93181818, pr2=0.970588, pr3=1)
 
 # parametrization of connections
-evaporator_wf_in.set_attr(T=111.6, fluid={'water': 0, 'Isopentane': 1})
-evaporator_wf_out.set_attr(T=119.8, p=10.8, state='g')
+evaporator_wf_in.set_attr(T=111.6, p=10.8, m=243.72, fluid={'water': 0, 'Isopentane': 1})
+evaporator_wf_out.set_attr(T=119.8)
 
-evaporator_steam_in.set_attr(T=146.6, p=4.4, state='g', fluid={'water': 1, 'Isopentane': 0})
+evaporator_steam_in.set_attr(T=146.6, p=4.4, m=21, fluid={'water': 1, 'Isopentane': 0})
 evaporator_sink_s.set_attr(T=132.5)
 
-evaporator_brine_in.set_attr(T=146.6, p=10.2, m=191, fluid={'water': 1, 'Isopentane': 0})
+evaporator_brine_in.set_attr(T=146.6, p=10.2, fluid={'water': 1, 'Isopentane': 0})
 evaporator_sink_b.set_attr(T=118.6)
 # solving
 mode = 'design'
--- Heat_exchanger_preheater.py
+++ Heat_exchanger_preheater.py
@@ -25,24 +25,26 @@ sink_b = sink('brine sink')
 # connections
 # main cycle
 # geo-fluid
-preheater_wf_in = connection(source_wf, 'out1', preheater, 'in2', m=243.72)
+preheater_wf_in = connection(source_wf, 'out1', preheater, 'in2')
 preheater_wf_out = connection(preheater, 'out2', sink_wf, 'in1')
 brine_in = connection(source_b, 'out1', preheater, 'in1')
 brine_out = connection(preheater, 'out1', sink_b, 'in1')
-nw.add_conns(preheater_wf_in, preheater_wf_out, brine_in, brine_out)
+nw.add_conns(preheater_wf_in, preheater_wf_out)
+nw.add_conns(brine_in, brine_out)
+#nw.add_conns(preheater_wf_in, preheater_wf_out, brine_in, brine_out)
 # parametrization of components
 preheater.set_attr(pr1=0.949494949494, pr2=0.955752212)
 #p_and_e.set_attr(Q=-9.365e7)
 # parametrization of connections
-preheater_wf_in.set_attr(T=39.7, p=11.3, fluid={'water': 0, 'Isopentane': 1})
+preheater_wf_in.set_attr(T=39.7, p=11.3, m=243.72, fluid={'water': 0, 'Isopentane': 1})
 preheater_wf_out.set_attr(T=111.6)
 
-brine_in.set_attr(T=119.9, fluid={'water': 1, 'Isopentane': 0})
-brine_out.set_attr(T=69.1, p=9.4)
+brine_in.set_attr(T=119.9, p=9.9, fluid={'water': 1, 'Isopentane': 0})
+brine_out.set_attr(T=69.1)
 # solving
 mode = 'design'
 file = 'yangyi_preheater'
 # solve the network, print the results to prompt and save
 nw.solve(mode=mode)
 nw.print_results()
-nw.save(file)
+#nw.save(file)
