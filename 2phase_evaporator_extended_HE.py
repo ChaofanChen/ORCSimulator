36757d11231a2196f88f5aacd178ec7c7745e8fa
ChaofanChen <chaofan.chen@ufz.de>
2020-01-31 15:44:07 +0100

    evaporator with hard BC outside, done.

--- 2phase_evaporator_extended_HE.py
+++ 2phase_evaporator_extended_HE.py
@@ -45,7 +45,7 @@ evaporator.set_attr(pr1=0.93181818, pr2=0.970588, pr3=1)
 evaporator_wf_in.set_attr(T=111.6, p=10.8, m=243.72, fluid={'water': 0, 'Isopentane': 1})
 evaporator_wf_out.set_attr(T=119.8, state='g')
 
-evaporator_steam_in.set_attr(T=146.6, p=4.4, m=21, fluid={'water': 1, 'Isopentane': 0})
+evaporator_steam_in.set_attr(T=146.6, p=4.33, m=20, state='g', fluid={'water': 1, 'Isopentane': 0})
 evaporator_sink_s.set_attr(T=132.5)
 
 evaporator_brine_in.set_attr(T=146.6, p=10.2, fluid={'water': 1, 'Isopentane': 0})
