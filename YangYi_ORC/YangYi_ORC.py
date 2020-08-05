bdec84afb246e594634c11410a8d9b8bd16c9bdf
ChaofanChen <chaofan.chen@ufz.de>
2020-08-05 14:51:41 +0200

    Add plotting T-s of ORC cycle.

--- YangYi_ORC/YangYi_ORC.py
+++ YangYi_ORC/YangYi_ORC.py
@@ -4,12 +4,18 @@ from tespy.networks import network
 from tespy.components import heat_exchanger, pump, turbine, source, sink, cycle_closer, splitter, merge, condenser
 from tespy.components.customs import orc_evaporator
 from CoolProp.CoolProp import PropsSI
+import matplotlib.pyplot as plt
+from CoolProp.Plots import PropertyPlot
+import CoolProp as CP
+import pandas as pd
 import numpy as np
 from tespy.tools import logger
 import logging
 mypath = logger.define_logging(
 log_path=True, log_version=True, timed_rotating={'backupCount': 4},
 screen_level=logging.WARNING, screen_datefmt = "no_date")
+import CoolProp
+print('CoolProp ver:%s'%(CoolProp.__version__))
 # define basic cycle
 fluids = ['water', 'Isopentane', 'Air']
 nw = network(fluids=fluids)
@@ -19,7 +25,7 @@ nw.set_attr(p_unit='bar', T_unit='C', h_unit='kJ / kg')
 # geo-fluid part
 # mass_flow_rate_brine = 190.56 # kg/s
 volume_flow_rate_brine = 700.23 # m3/h
-# mass_flow_rate_steam = 20.28
+mass_flow_rate_steam = 20.28
 T_brine_in = 144.8
 T_reinjection = 70.8
 # cooling air part
@@ -104,24 +110,17 @@ condenser.set_attr(pr1=0.8889, pr2=1)
 # parametrization of connections
 preheater_evaporator.set_attr(p=p_before_turbine, fluid={'water': 0, 'Isopentane': 1, 'Air': 0})
 
-evaporator_steam_in.set_attr(T=T_brine_in, p=p_steam_in, state='g', fluid={'water': 1, 'Isopentane': 0, 'Air':0})
+evaporator_steam_in.set_attr(T=T_brine_in, p=p_steam_in, m=mass_flow_rate_steam, state='g', fluid={'water': 1, 'Isopentane': 0, 'Air':0})
 evaporator_brine_in.set_attr(T=T_brine_in, m=mass_flow_rate_brine, fluid={'water': 1, 'Isopentane': 0, 'Air':0})
 preheater_sink.set_attr(T=T_reinjection)
 # evaporator_pump.set_attr(Td_bp=-5)
-evaporator_sink_b.set_attr(T=T_brine_in-22)
+# evaporator_sink_b.set_attr(T=T_brine_in-22)
 
 # air cooling connections
 ca_in.set_attr(T=T_air, p=p_air, fluid={'water': 0, 'Isopentane': 0, 'Air': 1})
-ca_out.set_attr(T=T_air + 15)
+# ca_out.set_attr(T=T_air + 15)
 
-# solving
-mode = 'design'
-save_path = 'yangyi'
-# solve the network, print the results to prompt and save
-# nw.solve(mode=mode)
-# nw.print_results()
-# nw.save(save_path)
-
+# parametrization of components
 # The parameter "ttd_u" will
 # constrain the temperature difference between
 # the hot inlet and the cold outlet.
@@ -133,6 +132,83 @@ ihe.set_attr(ttd_u=23.2)
 # difference is constrained. The value is based on
 # Yangyi monitoring data.
 preheater.set_attr(ttd_u=5.1)
-
+condenser.set_attr(ttd_u=6.98)
+# solving
+mode = 'design'
+save_path = 'yangyi'
 nw.solve(mode=mode, init_path=save_path)
 nw.print_results()
+
+thermal_efficiency = -turbine.P.val/1000/(evaporator_steam_in.h.val*mass_flow_rate_steam+evaporator_brine_in.h.val*mass_flow_rate_brine-preheater_sink.h.val*(mass_flow_rate_steam+mass_flow_rate_brine))
+print('Power_output (MW):', turbine.P.val / 1e6)
+print('Thermal_efficiency (%):', thermal_efficiency*100)
+##%-----------------------------------------------------------------------------------------------------------
+s_before_turbine = PropsSI('S', 'T', evaporator_turbine.T.val + 273.15, 'Q', 1, 'Isopentane')
+T_before_turbine = evaporator_turbine.T.val
+
+s_after_turbine = PropsSI('S', 'T', turbine_ihe.T.val + 273.15, 'P', turbine_ihe.p.val * 100000, 'Isopentane')
+T_after_turbine = turbine_ihe.T.val
+
+s_before_condenser = PropsSI('S', 'T', ihe_condenser.T.val + 273.15, 'P', ihe_condenser.p.val * 100000, 'Isopentane')
+T_before_condenser = ihe_condenser.T.val
+
+s_after_condenser = PropsSI('S', 'T', condenser_pump.T.val + 273.15, 'Q', 0, 'Isopentane')
+T_after_condenser = condenser_pump.T.val
+
+s_after_pump = PropsSI('S', 'T', pump_ihe.T.val + 273.15, 'P', pump_ihe.p.val * 100000, 'Isopentane')
+T_after_pump = pump_ihe.T.val
+
+s_after_ihe = PropsSI('S', 'T', ihe_wf_out.T.val + 273.15, 'P', ihe_wf_out.p.val * 100000, 'Isopentane')
+T_after_ihe = ihe_wf_out.T.val
+
+s_after_preheater = PropsSI('S', 'T', preheater_evaporator.T.val + 273.15, 'P', preheater_evaporator.p.val * 100000, 'Isopentane')
+T_after_preheater = preheater_evaporator.T.val
+
+state = CP.AbstractState('HEOS', 'Isopentane')
+T_crit = state.trivial_keyed_output(CP.iT_critical)
+df = pd.DataFrame(columns=['s_l', 's_g', 's_iso_P0', 's_iso_P1', 's_iso_P_top', 's_iso_P_bottom'])
+P0 = condenser_pump.p.val * 100000
+P1 = p_before_turbine * 100000
+T_range = np.geomspace(273.15, T_crit, 1000)
+for T in T_range:
+    df.loc[T, 's_l'] = PropsSI('S', 'T', T, 'Q', 0, 'Isopentane')
+    df.loc[T, 's_g'] = PropsSI('S', 'T', T, 'Q', 1, 'Isopentane')
+    df.loc[T, 's_iso_P0'] = PropsSI('S', 'T', T, 'P', P0, 'Isopentane')
+    df.loc[T, 's_iso_P1'] = PropsSI('S', 'T', T, 'P', P1, 'Isopentane')
+
+T_range_evaporator = np.geomspace(preheater_evaporator.T.val + 273.15, evaporator_turbine.T.val + 273.15+0.1, 100)
+for T in T_range_evaporator:
+    df.loc[T, 's_iso_P_top'] = PropsSI('S', 'T', T, 'P', P1, 'Isopentane')
+
+T_steam_wf_low_P = PropsSI('T', 'P', P0, 'Q', 1, 'Isopentane')
+s_steam_wf_low_P = PropsSI('S', 'P', P0, 'Q', 1, 'Isopentane')
+T_range_condenser = np.geomspace(T_steam_wf_low_P + 0.1, condenser_pump.T.val + 273.15-0.1, 100)
+for T in T_range_condenser:
+    df.loc[T, 's_iso_P_bottom'] = PropsSI('S', 'T', T, 'P', P0, 'Isopentane')
+# print(df)
+
+fig, ax = plt.subplots()
+ax.plot(df['s_g'], df.index - 273.15, color='black')
+ax.plot(df['s_l'], df.index - 273.15, color='black')
+ax.plot(df['s_iso_P0'], df.index - 273.15, color='green')
+ax.plot(df['s_iso_P1'], df.index - 273.15, color='green')
+ax.plot(df['s_iso_P_top'], df.index - 273.15, color='red')
+ax.plot(df['s_iso_P_bottom'], df.index - 273.15, color='red')
+
+Temp = [T_before_turbine, T_after_turbine, T_before_condenser, T_steam_wf_low_P-273.15, T_after_condenser, T_after_ihe, T_after_preheater]
+entropy = [s_before_turbine, s_after_turbine, s_before_condenser, s_steam_wf_low_P, s_after_condenser, s_after_ihe, s_after_preheater]
+n = ['before turbine', 'after turbine', 'before condenser', ' ', 'after condenser', 'after ihe', 'after preheater']
+
+ax.scatter(entropy, Temp, color='red')
+for i, txt in enumerate(n):
+    ax.annotate(txt, (entropy[i], Temp[i])) #, ha='center'
+for i in range (0, 3, 1):
+    plt.plot(entropy[i:i+2], Temp[i:i+2], 'ro-', lw=2)
+for i in range (4, 6, 1):
+    plt.plot(entropy[i:i+2], Temp[i:i+2], 'ro-', lw=2)
+
+ax.set(xlabel='Specific entropy [J/kg K]', ylabel='Temperature [dC]',
+       title='T,s Graph for working fluid')
+ax.grid()
+plt.savefig('ts_plot_with_ORC_cycle.png')
+plt.show()
