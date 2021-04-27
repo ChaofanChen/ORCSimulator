9d77d747a8f5cac3f1006549627e808a70ccf7fc
ChaofanChen <chaofan.chen@ufz.de>
2021-04-27 12:16:41 +0200

    Add plot for section 5.3.

--- parameter_influence.py
+++ parameter_influence.py
@@ -2,14 +2,55 @@ import matplotlib.pyplot as plt
 from geothermal_orc_design import single_parameter_influence
 from collections import OrderedDict
 import pandas as pd
+import numpy as np
 import itertools
 
 import json
 import sys
 
+#cur_dir = sys.argv[1] if len(sys.argv) > 1 else '.'
+#
+#with open(cur_dir + '/T_tur_influence.json', 'r') as f:
+#    input_data = json.load(f)
+#    f.close()
+#
+#result = single_parameter_influence(**input_data)
+#
+#print(result)
+#
+#### plot figures
+#fluid=list(result.keys())
+#
+#for a in range(len(fluid)):
+#    df = pd.DataFrame(list(result.values())[a])
+#    df['gross power output'] = -df['gross power output']/1e6
+#    df['net power output'] = -df['net power output']/1e6
+#
+#    fig, ax = plt.subplots(figsize=(8, 6), dpi=100)
+#    ax.plot(df['T_1'], df['gross power output'], color='blue', marker="o")
+#    ax.set(xlabel= 'Turbine inlet temperature with ' + fluid[a] + ' (°C)', ylabel='Gross power output (MW)')
+#    plt.ylim(5, 18)
+#    ax2=ax.twinx()
+#    ax2.plot(df['T_1'], df['T_35'], color='black', marker="*", label='Re-injection temperature')
+#    ax2.set(ylabel='Re-injection temperature (°C)')
+#    plt.ylim(40, 100)
+#    plt.xlim(50, 130)
+#    ax.yaxis.label.set_color('blue')
+#    ax.yaxis.label.set_size(18)
+#    ax.xaxis.label.set_size(18)
+#    ax.tick_params(axis="x", labelsize=15)
+#    ax.tick_params(axis="y", labelsize=15)
+#    ax.tick_params(axis='y', colors='blue')
+#    ax2.yaxis.label.set_size(18)
+#    ax2.tick_params(axis="y", labelsize=15)
+#    ax.grid()   
+#    plt.savefig('T_tur_with_' + fluid[a] + '.pdf')
+
+#%%   IHE sizing factor
+
 cur_dir = sys.argv[1] if len(sys.argv) > 1 else '.'
 
-with open(cur_dir + '/T_tur_influence.json', 'r') as f:
+with open(cur_dir + '/IHE_sizing_influence.json', 'r') as f:
     input_data = json.load(f)
     f.close()
 
@@ -19,21 +60,21 @@ print(result)
 
 ### plot figures
 fluid=list(result.keys())
-a = 0
+
 for a in range(len(fluid)):
     df = pd.DataFrame(list(result.values())[a])
     df['gross power output'] = -df['gross power output']/1e6
     df['net power output'] = -df['net power output']/1e6
 
     fig, ax = plt.subplots(figsize=(8, 6), dpi=100)
-    ax.plot(df['T_1'], df['gross power output'], color='blue', marker="o")
-    ax.set(xlabel= 'Turbine inlet temperature with ' + fluid[a] + ' (°C)', ylabel='Gross power output (MW)')
-    plt.ylim(5, 18)
+    ax.plot(df['IHE sizing factor'], df['net power output'], color='blue', marker="o")
+    ax.set(xlabel= 'IHE sizing factor with ' + fluid[a], ylabel='Net power output (MW)')
+    plt.ylim(6, 14)
     ax2=ax.twinx()
-    ax2.plot(df['T_1'], df['T_35'], color='black', marker="*", label='Re-injection temperature')
+    ax2.plot(df['IHE sizing factor'], df['T_35'], color='black', marker="*", label='Re-injection temperature')
     ax2.set(ylabel='Re-injection temperature (°C)')
     plt.ylim(40, 100)
-    plt.xlim(50, 130)
+    plt.xlim(0, 1)
     ax.yaxis.label.set_color('blue')
     ax.yaxis.label.set_size(18)
     ax.xaxis.label.set_size(18)
@@ -42,9 +83,10 @@ for a in range(len(fluid)):
     ax.tick_params(axis='y', colors='blue')
     ax2.yaxis.label.set_size(18)
     ax2.tick_params(axis="y", labelsize=15)
+    ax2.plot(np.linspace(0, 1, 5), (70,70,70,70,70), dashes=[2, 2], linewidth=3, color='red')
     ax.grid()   
     plt.savefig('T_tur_with_' + fluid[a] + '.pdf')
-    a += 1
+
     
 
 #%%
